0f2528f06451ea01742e9e08b8c28ddb43dd23c0
XEVO <marcrommelagravante21@gmail.com>
2024-11-21 20:23:23 +0800

    MOVE CART DATA TO ORDER TABLE-26

--- grindonweb-main/resources/views/home/mycart.blade.php
+++ grindonweb-main/resources/views/home/mycart.blade.php
@@ -32,6 +32,25 @@
             border: 1px solid skyblue;
         }
 
+        .cart_value{
+            text-align: center;
+            margin-bottom: 70px;
+            padding: 18px;
+        }
+
+        .order_deg{
+            padding-right: 100px;
+            margin-top: -50px;
+        }
+        label{
+            display: inline-block;
+            width: 150px;
+        }
+
+        .div_gap{
+            padding: 20px;
+        }
+
     </style>
 
 </head>
@@ -47,6 +66,47 @@
 
     <div class="div_deg">
 
+
+        <div class="order_deg">
+
+            <form action="{{url('confirm_order')}}" method="POST">
+                @csrf
+
+                <div class="div_gap">
+
+                    <label>Receiver Name</label>
+
+                    <input type="text" name="name" value="{{Auth::user()->name}}">
+
+                </div>
+
+                <div class="div_gap">
+
+                    <label>Receiver Address</label>
+
+                    <textarea name="address">{{Auth::user()->address}}</textarea>
+
+                </div>
+
+                <div class="div_gap">
+
+                    <label>Receiver Phone</label>
+
+                    <input type="text" name="phone" value="{{Auth::user()->phone}}">
+
+                </div>
+
+                <div class="div_gap">
+
+                    <input class="btn btn-primary" type="submit" value="Place Order">
+
+                </div>
+
+            </form>
+
+        </div>
+
+
         <table>
 
             <tr>
@@ -55,7 +115,15 @@
                 <th>Price</th>
 
                 <th>Image</th>
+
+                <th>Remove</th>
             </tr>
+
+            <?php
+
+            $value = 0;
+
+            ?>
 
             @foreach($cart as $cart)
 
@@ -69,7 +137,17 @@
                     <img width="150" src="/products/{{$cart->product->image}}">
                 </td>
 
+                <td>
+                    <a class="btn btn-danger" href="{{url('delete_cart',$cart->id)}}">Remove</a>
+                </td>
+
             </tr>
+
+            <?php
+
+            $value = $value + $cart->product->price;
+
+            ?>
 
             @endforeach
 
@@ -77,6 +155,9 @@
 
     </div>
 
+    <div class="cart_value">
+        <h3>Total Value of Cart is: PHP {{$value}}</h3>
+    </div>
 
 
   <!-- info section -->
